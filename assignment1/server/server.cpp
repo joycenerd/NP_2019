dc976ae10269788c1cc6439fbe228bebe0a8b67d
joycenerd <joycenerd@csie.io>
2019-10-29 21:50:35 +0800

    Write a simple web server that can show your html on Chrome     4. Create socket     5. Setup the network connection     6. Create and start listening to network     7. Patrol all the socket in active fd set

--- assignment1/server/server.cpp
+++ assignment1/server/server.cpp
@@ -10,6 +10,7 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <string>
+#include <iostream>
 #define BUFSIZE 8192
 
 using namespace std;
@@ -35,9 +36,80 @@ struct {
 const char *CLIENT_FOLDER="../";
 
 int main(int argc,char **argv) {
+    int listenfd,ret;
+    static struct sockaddr_in serverAddr,clientAaddr;
+    fd_set activefdSet,readfdSet;
+
+
     // go to the folder that has the .html file
     if(chdir(CLIENT_FOLDER)==-1) {
         printf("ERROR: Cannot change to directory %s\n",CLIENT_FOLDER);
         exit(4);
     }
+
+
+    // create socket
+    listenfd=socket(AF_INET,SOCK_STREAM,0);
+
+    if(listenfd<0){
+        perror("ERROR: opening socket\n");
+        exit(3);
+    }
+
+    cout << "listenfd: " << listenfd << endl;
+
+    // setup the network connection
+
+    /*struct sockaddr_in {
+        sa_family_t    sin_family;  address family: AF_INET 
+        in_port_t      sin_port;    port in network byte order 
+        struct in_addr sin_addr;    internet address 
+    };*/
+
+    serverAddr.sin_family=AF_INET;
+    serverAddr.sin_addr.s_addr=htonl(INADDR_ANY);
+    serverAddr.sin_port=htons(8080);
+
+    // create and start listening to network
+    ret=bind(listenfd,(struct sockaddr *)&serverAddr,sizeof(serverAddr));
+
+    if(ret<0){
+        perror("ERROR: opening listening\n");
+        exit(3);
+    }
+
+    if(listen(listenfd,1)<0){
+        perror("ERROR: start listening to network\n");
+        exit(3);
+    }
+
+    FD_ZERO(&activefdSet);
+    FD_SET(listenfd,&activefdSet);
+
+    while(1) {
+        readfdSet=activefdSet;
+
+        if(select(FD_SETSIZE,&readfdSet,NULL,NULL,NULL)<0) {
+            perror("ERROR: select");
+            exit(EXIT_FAILURE);
+        }
+
+        // patrol all the socket in fd_set
+        
+        for(int i=0;i<FD_SETSIZE;i++) {
+            if(FD_ISSET(i,&readfdSet)) {
+                if(i==listenfd) {
+                    socklen_t addrLength=sizeof(clientAaddr);
+                    int newfd=accept(listenfd,(struct sockaddr *)&clientAaddr,&addrLength);
+                    if(newfd<0){
+                        perror("ERROR: accept\n");
+                        exit(EXIT_FAILURE);
+                    }
+                    FD_SET(newfd,&activefdSet);
+                }
+            }
+        }
+    }
+
+    return 0;
 }
