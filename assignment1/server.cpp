bf6d62694ec3883b93fa3c402779509217cf3d71
joycenerd <joycenerd@csie.io>
2019-10-29 12:46:03 +0800

    1. Restart the project 2. Add my html file as client 3. Add my server

--- assignment1/server.cpp
+++ assignment1/server.cpp
@@ -1,5 +1,13 @@
-#include "./include/HttpGetPostMethod.h"
+#include <cstdio>
+#include <cstdlib>
+#include <cunistd>
+#include <cerrno>
+#include <cstring>
+#include <cfcntl>
+#include <csignal>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#define BUFSIZE 8192
 
-int main() {
-    return 0;
-}
